189b53a8032ffc7066e978c1dd687901b2ed3ba0
ghadir <ghadirfakih164@gmail.com>
2025-04-20 10:09:17 +0300

    resolve merge conflict in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,38 +9,27 @@ import MyAppointments from "./pages/MyAppointments";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Fotter";
 import Appointment from "./pages/Appointment";
-function App() {
 
+function App() {
   return (
     <>
-    <div className="mx-4 sm:mx-[10%]">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/doctors" element={<Doctor />} />
-<<<<<<< HEAD
-=======
-
->>>>>>> 0e76cb1dbf2520367a084f052dce0a9cb8d8c25b
-        <Route path="/doctors/:speciality" element={<Doctor />} />
-       <Route path="/login" element={<Login />} />
-       <Route path="/about" element={<About />} />
-       <Route path="/contact" element={<Contact />} />
-       <Route path="/my-profile" element={<MyProfile />} />
-<<<<<<< HEAD
-       <Route path="/my-appoitments" element={<MyAppointments />} />
-      <Route path="/appointment/:docId" element={<Appointment />} />
-=======
-       <Route path="/my-appointments" element={<MyAppointments />} />
-       <Route path="/appoitment/:docId" element={<Appointment />} />
-      </Routes>
->>>>>>> 0e76cb1dbf2520367a084f052dce0a9cb8d8c25b
-
-      </Routes>
+      <div className="mx-4 sm:mx-[10%]">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/doctors" element={<Doctor />} />
+          <Route path="/doctors/:speciality" element={<Doctor />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/my-profile" element={<MyProfile />} />
+          <Route path="/my-appointments" element={<MyAppointments />} />
+          <Route path="/appointment/:docId" element={<Appointment />} />
+        </Routes>
         <Footer />
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
