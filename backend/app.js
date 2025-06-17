fe39b4dea8fb6210a94a5c6c6135b6824a911878
ghadir <ghadirfakih164@gmail.com>
2025-06-17 15:28:47 +0300

    app cors

--- backend/app.js
+++ backend/app.js
@@ -10,20 +10,9 @@ const DoctorRoutes=require('./src/routes/DoctorRoutes');
 const StaffRoutes=require('./src/routes/StaffRoutes');
 const chatRoutes = require("./src/controllers/ChatControllers");
 const app = express();
-<<<<<<< HEAD
+
 app.use(cors());
-// ✅ Enhanced CORS setup
-// app.use(cors({
-//   origin: 'http://localhost:5175', // React Vite frontend
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true // only needed if using cookies/auth headers
-// }));
 
-// ✅ Middlewares
-=======
-
->>>>>>> 1d83ae6e21cf6ecac8989dac97f539d94c7ea031
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname,'src','uploads')));
